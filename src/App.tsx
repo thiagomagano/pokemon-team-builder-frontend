eba8bd484931b02f8c043359aef1ab494d89008d
Thiago Magano <thiagomagano1993@outlook.com>
2022-04-22 20:37:51 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div>
+      Hello World!
+      {count}
+      <button onClick={() => setCount(count + 1)}>count</button>
+    </div>
+  )
+}
+
+export default App
