611e31d0a559c6b55e0dba8a7d27798a90b20ba8
Thiago Magano <thiagomagano1993@outlook.com>
2022-04-26 21:57:21 -0300

    wip: add api request to types and pokemons on pokedex

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import api from "../services/api";
 
-function Pokedex() {
-  return <div>Pokedex</div>;
+export default function Pokedex() {
+  const [types, setTypes] = useState("");
+  const [pokemons, setPokemons] = useState("");
+
+  async function getAllTypes() {
+    const response = await api.get("/types");
+    const data = await response?.data;
+    setTypes(data);
+  }
+
+  async function getAllPokemons() {
+    const response = await api.get("/pokemons");
+    const data = await response?.data;
+    setPokemons(data);
+  }
+
+  useEffect(() => {
+    getAllTypes();
+    getAllPokemons();
+  }, []);
+
+  return (
+    <div className="pokedex">
+      <div className="pokedex-title">
+        <h2>Pokedex</h2>
+      </div>
+
+      <div className="filter">
+        <div className="filter-types">
+          <h3>Types</h3>
+          <div className="checkbox-group">
+            {types &&
+              types.map((type) => {
+                return <TypeCheckBox type={type} />;
+              })}
+          </div>
+        </div>
+        <div className="filter name">
+          <input type="text" placeholder="Pesquise Por Nome" />
+        </div>
+        <button>Filtrar</button>
+      </div>
+
+      <ul className="pokedex-list">
+        {pokemons &&
+          pokemons.map((pokemon) => {
+            return <PokemonCard pokemon={pokemon} />;
+          })}
+      </ul>
+    </div>
+  );
 }
 
-export default Pokedex;
+function TypeCheckBox({ type }) {
+  return (
+    <div className="checkbox-item">
+      <input
+        className="filter-checkbox"
+        type="checkbox"
+        name={type.name}
+        id={type.name}
+        value={type.name}
+      />
+      <label htmlFor={type.name}> {type.name}</label>
+    </div>
+  );
+}
+
+function PokemonCard({ pokemon }) {
+  return (
+    <li className="pokemon-card">
+      <ul>
+        <li>
+          <img
+            className="card-image"
+            src={pokemon.avatarUrl}
+            alt={pokemon.name}
+          />
+        </li>
+        <li>#{pokemon.id}</li>
+        <li>{pokemon.name}</li>
+      </ul>
+    </li>
+  );
+}
