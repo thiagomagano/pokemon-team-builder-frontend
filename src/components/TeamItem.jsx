df0c7f2b8994f224ad102ac1e21f9c78f8b0450e
Thiago Magano <thiagomagano1993@outlook.com>
2022-04-29 13:06:35 -0300

    frist tests on damage relations

--- src/components/TeamItem.jsx
+++ src/components/TeamItem.jsx
@@ -1,7 +1,41 @@
 import useTeam from "../hooks/useTeam";
+import axios from "axios";
+import { useEffect } from "react";
+
+const pokeapi = axios.create({
+  baseURL: "https://pokeapi.co/api/v2/type/",
+});
+
+const API_ENDPOINT = "https://pokeapi.co/api/v2/type";
 
 export default function TeamItem({ pokemon, position }) {
   const { team, setTeam } = useTeam();
+
+  async function getDamageRelation(type) {
+    const response = await pokeapi.get(type.name);
+    const data = response?.data;
+
+    const veryGoodAgaist = data.damage_relations.double_damage_to[0]?.name;
+    const veryBadAgaist = data.damage_relations.double_damage_from[0]?.name;
+
+    const goodAgaist = data.damage_relations.half_damage_to[0]?.name;
+    const badAgaist = data.damage_relations.half_damage_from[0]?.name;
+
+    const superBadAgaist = data.damage_relations.no_damage_to[0]?.name;
+    const superGoodAgaist = data.damage_relations.no_damage_from[0]?.name;
+
+    console.log(`Muito Bom Contra: ${veryGoodAgaist || "xxxxxxxx"}`);
+    console.log(`Muito Ruim Contra: ${veryBadAgaist || "xxxxxxxx"}`);
+    console.log(`Bom Contra: ${badAgaist || "xxxxxxxx"}`);
+    console.log(`Ruim Contra: ${goodAgaist || "xxxxxxxx"}`);
+    console.log(`Não sofre dano: ${superGoodAgaist || "xxxxxxxx"}`);
+    console.log(`Não dá dano: ${superBadAgaist || "xxxxxxxx"}`);
+  }
+
+  useEffect(() => {
+    if (pokemon) getDamageRelation(pokemon.types[0]);
+  }, [pokemon]);
+
   return pokemon ? (
     <li className="team-item">
       <div className="pokemon-avatar">
